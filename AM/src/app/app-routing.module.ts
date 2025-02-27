e967b06992fd11478a4f500f05047964522ed725
brickstate <brickersh@appstate.edu>
2025-02-27 16:34:11 -0500

    got a kinda nav bar element in the website no functions or linking to any other pages

--- AM/src/app/app-routing.module.ts
+++ AM/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  //{ path: 'about', component: AboutComponent },
+  //{ path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
--- AM/src/app/shared/navbar/navbar.component.ts
+++ AM/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  @ViewChild('menu') mainNav!: ElementRef;
+  @ViewChild('toggle') navBarToggle!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    if (this.navBarToggle && this.mainNav) {
+      this.renderer.listen(this.navBarToggle.nativeElement, 'click', () => {
+        this.mainNav.nativeElement.classList.toggle('active');
+      });
+    }
+  }
 }
